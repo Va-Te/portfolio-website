2a80b4541cf8100bc95fb1847e2034b14ed797a5
Va-Te <158156967+Va-Te@users.noreply.github.com>
2024-02-14 17:00:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e) {
+
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    //cursorOutline.style.left = `${posX}px`;
+    //cursorOutline.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+        left:`${posX}px`,
+        top: `${posY}px`
+    }, { duration: 500, fill: "forwards" });  
+
+    let links = Array.from(document.querySelectorAll("a"));
+
+    console.log(links);
+
+    links.forEach(link => {
+        link.addEventListener('mouseover', ()=>{
+          cursorDot.classList.add("grow");  
+        })
+        link.addEventListener('mouseleave', ()=>{
+          cursorDot.classList.remove("grow");  
+        })
+    });
+
+});
